806547d0ed885a6d8f5727d61b4d8c221182f8cf
fatin-israq <israqq2120@gmail.com>
2025-05-17 01:29:18 +0600

    Added filter() and map() functions use case. Refactor setInterval and setTimeout to use let for intervalID management.

--- Advanced Functions/12-advanced-functions.js
+++ Advanced Functions/12-advanced-functions.js
@@ -26,9 +26,13 @@ setTimeout(function () {
   console.log("timeout");
 }, 3000);
 
-setInterval(function () {
+let intervalID = setInterval(function () {
   console.log("interval");
 }, 3000);
+
+setTimeout(function () {
+  clearInterval(intervalID);
+}, 15000);
 
 console.log("next-line-2");
 
@@ -37,3 +41,41 @@ fruits.forEach(function (value, index) {
   console.log(index);
   console.log(value);
 });
+
+
+
+// Some more functions
+
+
+// filter ((value, index) => {
+//  });
+// It can return a value, if the value is truthful then it returns the value of the array and store it in the new array, otherwise it doesn't include the value in that array.
+// It kind of works like forEach loop.
+
+console.log(
+  [1, -3, 5].filter((value, index) => {
+    if (value >= 0) {
+      return true;
+    } else {
+      return false;
+    }
+
+    // return value >= 0; does the same thing as above
+  })
+); // its going to return a new array which is [1, 5]
+
+
+// map function
+// Creates a new array[]
+// Whatever we return => added to new array
+console.log([1, 1, 3].map((value, index) => {
+  return value * 2;
+}))
+
+// Creating more shortcut way to write the above code
+console.log([1, 1, 3].map(value => value * 3));
+
+
+// Closure function
+// If a function has access to a value
+// It will always have access to that value
